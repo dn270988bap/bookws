9160046283fcc344917332c3113466ede06a7429
dn270988bap <transcendental.sc@gmail.com>
2017-12-13 14:06:43 +0200

    Исправлен косяк в методе create

--- src/main/java/com/sb/bookws/dao/AuthorDao.java
+++ src/main/java/com/sb/bookws/dao/AuthorDao.java
@@ -9,34 +9,36 @@ public class AuthorDao {
     private int id = 0;
     private List<Author> authors = new ArrayList<>();
 
-    public void create(Author author) {
-
+    public int create(Author author) {
         if (author.getId() <= 0) {
             author.setId(getNextId());
-        } else if (-1 != findById(author.getId())) {
+        }
+        if (-1 != findById(author.getId())) {
             throw new RuntimeException("ID already exists");
         }
         authors.add(author);
+        return author.getId();
     }
 
     public void update() {
 
     }
 
-    public void remove(int id) {
-        if (-1 != findById(id)) {
-            authors.remove(findById(id));
+    public int remove(int id) {
+        int pos = findById(id);
+        if (-1 != pos) {
+            authors.remove(pos);
         }
+        return 1;
     }
 
     public int findById(int authorid) {
-        int pos = -1;
         for (int i = 0; i < authors.size(); i++) {
             if (authorid == authors.get(i).getId()) {
-                pos = i;
+                return i;
             }
         }
-        return pos;
+        return -1;
     }
 
     public int getNextId() {
