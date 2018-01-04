4ca6b606b75453d5bd8258f526ac271ba2e7f8a8
Eugen <Evgenij.Sedov@privatbank.ua>
2018-01-04 15:35:40 +0200

    Доработка BookDao

--- src/main/java/com/sb/bookws/dao/BookDao.java
+++ src/main/java/com/sb/bookws/dao/BookDao.java
@@ -4,49 +4,33 @@ import com.sb.bookws.entity.Book;
 import java.util.ArrayList;
 import java.util.List;
 
-public class BookDao {
-
+public class BookDao{
     private int id = 0;
-    List<Book> books = new ArrayList<>();
-
-    public void create(Book book) {
-        int cnt = 0;
-        if (book.getId() <= 0) {
-            book.setId(getNextId());
-            books.add(book);
-        } else {
-            for (int j = 0; j < books.size(); j++) {
-                if (book.getId() == books.get(j).getId()) {
-                    cnt++;
-                }
-            }
-            if (cnt == 0) {
-                books.add(book);
-            } else {
-                book.setId(getNextId());
-                books.add(book);
-            }
-
-        }
-
-    }
-
-    public void update() {
-
-    }
-
-    public void remove(int id) {
-        if (id > 0) {
-            for (int i = 0; i < books.size(); i++) {
-                if (books.get(i).getId() == id) {
-                    books.remove(i);
-                }
-            }
-        }
-    }
-
+    private List<Book> books = new ArrayList<>();
+    
     public int getNextId() {
         return ++id;
     }
+    
+    public int create (Book book) {
+        if (book.getId() <= 0){
+            book.setId(getNextId());
+        }
+        if (-1 != findById(book.getId())){
+            throw new RuntimeException("ID already exists");
+        }
+            books.add(book);
+            return book.getId();
+    }
+    public int findById(int bookid){
+        for (int i = 0; i < books.size(); i++){
+            if (bookid == books.get(i).getId()){
+                return i;
+            }
+        }
+        return -1;
+    } 
 
-}
+    
+  }
+
