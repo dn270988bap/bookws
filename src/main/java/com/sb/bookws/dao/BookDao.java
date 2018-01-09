7f3a2742d43c1fc36189ab5eb5436a87c5d1c4b2
Eugen <Evgenij.Sedov@privatbank.ua>
2018-01-09 10:55:58 +0200

    Commit

--- src/main/java/com/sb/bookws/dao/BookDao.java
+++ src/main/java/com/sb/bookws/dao/BookDao.java
@@ -4,33 +4,34 @@ import com.sb.bookws.entity.Book;
 import java.util.ArrayList;
 import java.util.List;
 
-public class BookDao{
+public class BookDao {
+
     private int id = 0;
     private List<Book> books = new ArrayList<>();
-    
+
     public int getNextId() {
         return ++id;
     }
-    
-    public int create (Book book) {
-        if (book.getId() <= 0){
+
+    public int create(Book book) {
+        if (book.getId() <= 0) {
             book.setId(getNextId());
         }
-        if (-1 != findById(book.getId())){
+        if (-1 != findById(book.getId())) {
             throw new RuntimeException("ID already exists");
         }
-            books.add(book);
-            return book.getId();
+
+        books.add(book);
+        return book.getId();
     }
-    public int findById(int bookid){
-        for (int i = 0; i < books.size(); i++){
-            if (bookid == books.get(i).getId()){
+
+    public int findById(int bookid) {
+        for (int i = 0; i < books.size(); i++) {
+            if (bookid == books.get(i).getId()) {
                 return i;
             }
         }
         return -1;
-    } 
+    }
 
-    
-  }
-
+}
